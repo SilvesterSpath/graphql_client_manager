4b2edf7daafbad4c4b1d1ae48a5791d45bb0f268
SilvesterSpath <spath.sz@gmail.com>
2024-03-20 06:25:18 +0100

    sampleData

--- server/index.js
+++ server/index.js
@@ -1,4 +1,23 @@
+require('dotenv').config(); // this should be over the port
 const express = require('express');
-const PORT = process.env.PORT || 5000;
+const { graphqlHTTP } = require('express-graphql');
+
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: require('./schema/schema'),
+    graphiql: process.env.NODE_ENV === 'development' ? true : false,
+  })
+);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
